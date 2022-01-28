ad409e8b653512b744329b5921bad378ed8033ae
alfredovitale <alfredovitale@users.noreply.github.com>
2022-01-28 02:11:47 +0100

    Add files via upload

---
+++ src/testant/TestantClass.java
@@ -0,0 +1,18 @@
+package testant;
+
+/**
+* Klasse zum Testen der Ant Tasks
+* @author Alfredo Vitale
+ * @version 2.0
+ *
+ * */
+
+public class TestantClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World, how are you?");
+    }
+
+    public String getMessage(){
+        return "test message";
+    }
+}
